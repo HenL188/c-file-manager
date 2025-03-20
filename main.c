933db83cc773c5dbbe9b369ae696738e4bf8b9d1
HenL188 <153272326+HenL188@users.noreply.github.com>
2025-03-19 23:28:33 -0600

    almost finished file cmds

--- main.c
+++ main.c
@@ -9,12 +9,77 @@
 #define MAX_PATH 115
 
 /*
-  find, cat, touch, rm, mv
+    mv
   find dir, rmdir, mkdir, mv dir, pwd
 */
 
 void help(){
-  //read from help file
+  char line[50];
+  FILE *file = fopen("help.txt", "r");
+  if (file == NULL){
+    perror("Failed to open file");
+  }
+  else {
+    while(fgets(line, 50, file)!= NULL){
+      printf("%s\n", line);
+    }
+    fclose(file);
+  }
+}
+
+void filecmd(char cmd[],char path[], char *pathstart, char arg[]){
+  if (!strcmp(cmd, "cat")){
+    char line[256];
+    FILE *file = fopen(arg, "r");
+    if (file == NULL){
+      perror("Failed to open file");
+    }
+    else {
+      while(fgets(line, 256, file)!= NULL){
+      printf("%s\n", line);}
+      fclose(file);
+    }
+  }
+  if(!strcmp(cmd, "find")){
+      FILE *file = fopen(arg, "r");
+      if (file == NULL){
+        puts("File not found");
+      }
+      else {
+        puts("File found");
+        fclose(file);
+      }
+  }
+  if(!strcmp(cmd, "touch")){
+    FILE *file = fopen(arg, "r");
+    if (file == NULL){
+      fopen(arg, "w");
+      fclose(file);
+      puts("File created");
+    }
+    else {
+      puts("A file by that name aready exsit");
+    }
+  }
+  if(!strcmp(cmd, "rm")){
+    if(remove(arg) == 0) puts("File deleted");
+    else puts("File failed to delete");
+  }
+  if(!strcmp(cmd, "mv")){
+    
+  }
+}
+
+void dircmd(char cmd[],char path[], char *pathstart, char arg[]){
+  if (!strcmp(cmd, "ls")){
+      char dst[118];
+      utils_concat(path, MAX_PATH, pathstart, arg);
+      // for(int i = 0; i < 115; i++){
+      //   path[i] = arg[i];
+      // }
+      utils_concat(dst, MAX_PATH, "dir ", path);
+      if (system(dst) != 0 ) puts(strerror(errno));
+    }
 }
 
 int main(void){
@@ -34,21 +99,15 @@ int main(void){
       goto loop;
     }
     if (!strcmp(cmd, "quit") || !strcmp(cmd, "q")) exit(0);
-    if (!strcmp(cmd, "ls")){
-      char dst[118];
-      utils_concat(path, MAX_PATH, pathstart, arg);
-      utils_concat(dst, MAX_PATH, "dir ", path);
-      if (system(dst) != 0 ){
-       puts(strerror(errno));
-     }
-    }
     if (!strcmp(cmd, "clear")){
       if (system("cls") != 0 ) puts(strerror(errno)); 
     }
-    if(!strcmp(cmd, "set")){
-      pathstart = arg;
-      printf("Path set to %s\n", pathstart);
-    }
+    if (!strcmp(cmd, "help")) help();
+    
+    
+    dircmd(cmd, path,pathstart,arg);
+    filecmd(cmd, path, pathstart, arg);
+    
   }
   return 0;
 }
